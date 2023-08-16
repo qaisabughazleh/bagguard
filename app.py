f068d0718c8210d124e1129afc5e0640a85c9d66
qaisabughazleh <127531907+qaisabughazleh@users.noreply.github.com>
2023-08-16 23:06:17 +0300

    Update and rename app.py to app.php

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask,request,jsonify
-import logging
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-app =Flask(__name__)
-@app.route('/')
-def hello_world():
-  return "QAIS"
